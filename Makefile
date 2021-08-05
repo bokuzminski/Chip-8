1a81c90bbf558122babefbee37180d2bf8450b7a
bokuzminski <bogdankuzminski@gmail.com>
2021-08-05 15:42:08 +0200

    setup SDL window

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 INCLUDES= -I ./include
 FLAGS= -g
 all: 
-	gcc ${FLAGS} ${INCLUDES} ./src/main.c -o ./bin/main
+	gcc ${FLAGS} ${INCLUDES} ./src/main.c -lSDL -o  ./bin/main
--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <SDL/SDL.h>
+
+#undef main
+
+#define SCREEN_W 640
+#define SCREEN_H 320
+#define SCREEN_BPP 32
+#define W 64
+#define H 32
 
 int main(void)
 {
-    printf("Hello");
+    SDL_Event event;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_SetVideoMode(SCREEN_W, SCREEN_H, SCREEN_BPP, SDL_HWSURFACE | SDL_DOUBLEBUF);
+
+    while (1)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                exit(1);
+            }
+        }
+    }
+
     return 0;
 }
