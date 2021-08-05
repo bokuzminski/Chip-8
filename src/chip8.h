9dbff81218f56a8290ddcd159b11db99a534acbc
bokuzminski <bogdankuzminski@gmail.com>
2021-08-05 23:22:47 +0200

    added initialization

---
+++ src/chip8.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct Chip8
+{
+    u_int8_t memory[4096];
+    u_int8_t V[16]; // registers  from V0 to VF
+    u_int16_t index_register;
+    u_int16_t PC;
+    u_int16_t stack[16];
+    u_int8_t SP;
+    u_int8_t delay_timer;
+    u_int8_t sound_timer;
+    u_int32_t video[64 * 32];
+    u_int16_t opcode;
+} Chip8;
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
+#include "fontset.h"
+#include "chip8.h"
 
 #undef main
 
@@ -9,11 +11,21 @@
 #define W 64
 #define H 32
 
-int main(void)
+void chip8_initialization(Chip8 *ch8, FILE *file);
+
+int main(int argc, char *argv[])
 {
     SDL_Event event;
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_SetVideoMode(SCREEN_W, SCREEN_H, SCREEN_BPP, SDL_HWSURFACE | SDL_DOUBLEBUF);
+
+    Chip8 ch8;
+
+    FILE *game = fopen(argv[1], "rb");
+    if (game == NULL)
+    {
+        printf("Can't open the ROM");
+    }
 
     while (1)
     {
@@ -24,7 +36,26 @@ int main(void)
                 exit(1);
             }
         }
+        chip8_initialization(&ch8, game);
     }
 
     return 0;
+}
+
+void chip8_initialization(Chip8 *ch8, FILE *file)
+{
+    int i;
+
+    fread(ch8->memory + 0x200, 1, 4096 - 0x200, file);
+    for (i = 0; i < FONTSET_SIZE; i++)
+    {
+        ch8->memory[i] = fontset[i];
+    }
+    memset(ch8->video, 0, sizeof(ch8->video));
+    memset(ch8->stack, 0, sizeof(ch8->stack));
+    memset(ch8->V, 0, sizeof(ch8->V));
+
+    ch8->PC = 0x200;
+    ch8->SP &= 0;
+    ch8->opcode = 0x200;
 }
